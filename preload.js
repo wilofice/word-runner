ad1ee599124bfa02ca0322be4354ae9cfba735dd
wilofice <wilofice@gmail.com>
2023-06-11 03:28:07 +0200

    init electron app skeleton

---
+++ preload.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+    }
+  
+    for (const dependency of ['word']) {
+      replaceText(`${dependency}`, "word")
+    }
+  })
